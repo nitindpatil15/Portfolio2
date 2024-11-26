8f61a7e4ec435152efcfdf3e19afaaaa94d9af0f
nitindpatil15 <nitindpatil2002@gmail.com>
2024-11-26 09:32:10 +0530

    updates

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -9,23 +9,24 @@ const Footer = () => {
   return (
     <>
       <footer className="mt-5 bg-slate-800 p-2 flex items-center justify-center">
-        <div className="md:text-2xl text-base text-red-600 font-bold mx-2 md:mx-5">@Nitin D Patil</div>
+        <div className="md:text-2xl text-base text-red-600 font-bold mx-2 md:mx-5">
+          @Nitin D Patil
+        </div>
         <div className="flex flex-row">
-          <Link className="mx-2 w-5 md:w-8 rounded-full" to="https://github.com/nitindpatil15">
-          <img
-              className="my-2 text-white m-1"
-              src={LinkedIn}
-              alt="LinkedIn"
-            />
+          <Link
+            className="mx-2 w-5 md:w-8 rounded-full"
+            to="https://github.com/nitindpatil15"
+          >
+            <img className=" text-white m-1 my-2 " src={Github} alt="Github" />
           </Link>
           <Link
             className="mx-2 w-5 md:w-8 rounded-full"
             to="https://www.linkedin.com/in/nitin-patil-50249623b/"
           >
             <img
-              className=" text-white m-1 my-2 "
-              src={Github}
-              alt="Github"
+              className="my-2 text-white m-1"
+              src={LinkedIn}
+              alt="LinkedIn"
             />
           </Link>
           <Link
@@ -38,7 +39,10 @@ const Footer = () => {
               alt="Instagram"
             />
           </Link>
-          <Link className="mx-2 w-5 md:w-8 rounded-full" to="https://twitter.com/NitinPa42701682">
+          <Link
+            className="mx-2 w-5 md:w-8 rounded-full"
+            to="https://twitter.com/NitinPa42701682"
+          >
             <img
               className=" text-white my-2  m-1"
               src={Twitter}
